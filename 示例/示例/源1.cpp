264172189d09a315c006d32995f0bc50de280c88
“huxiangjie” <450590476@qq.com>
2018-01-04 16:33:35 +0800

    [for test]

---
+++ 示例/示例/源1.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	fstream log;
+//	log.open("E:\Log.txt",ios::in|ios::out|ios::binary);
+//	if (!log.is_open())
+//	{
+//		cout << "cant open log!" << endl;
+//	}
+//	string line;
+//	while (getline(log, line))
+//	{
+//		cout << line <<endl;
+//	}
+//	return 0;
+//}
